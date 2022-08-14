c5e1318fec6b218c00343a8075009b81be623593
Nguyễn Văn Ngọc <ngocnv.uet@gmail.com>
2022-08-14 17:05:28 +0700

    update
    create api_file gitignore

--- youtube_comments/youtube_comments.py
+++ youtube_comments/youtube_comments.py
@@ -1,12 +1,19 @@
 import json
+from socket import AI_ADDRCONFIG
 
 import requests
-VIDEO_ID=input("Enter ID: ")
+def get_api():
+    with open('youtube_comments/api_file.json', 'r') as openfile:
+    # Reading from json file
+        json_object = json.load(openfile)
+        return json_object
+
+API_KEY = get_api().get('API_KEY')
+print(API_KEY)
+VIDEO_ID=input("VIDEO_ID : ")
 URL="https://www.googleapis.com/youtube/v3/commentThreads"
-API_KEY="AIzaSyBZnBVHK5L57k4cwbEVPN7GXMKHOlmnW3U"
 
 #VIDEO_ID="tlcWiP7OLFI&list=PLhBgTdAWkxeCL3bUv6NLGrg2248ryIUAD"
-
 
 response=requests.get(f"{URL}?key={API_KEY}&videoId={VIDEO_ID}&part=snippet&part=replies")# add replies
 data=json.loads(response.text)
